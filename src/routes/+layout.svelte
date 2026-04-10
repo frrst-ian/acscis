a13b89baa1dc0cc04bdca8c994f98de4c5475fc5
frrst-ian <frrstxd@proton.me>
2026-04-11 06:39:31 +0800

    feat: add homepage

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,175 @@
+<script>
+	import '../app.css';
+	import logo from '$lib/assets/logo.png';
+	import { Calendars, Mail, Phone } from '@lucide/svelte';
+	import Icon from 'svelte-awesome';
+	import facebookOfficial from 'svelte-awesome/icons/facebookOfficial';
+
+	let { children } = $props();
+</script>
+
+<svelte:head>
+	<link rel="icon" href={logo} />
+</svelte:head>
+
+<nav>
+	<a class="logo" href="/"><img src={logo} alt="logo" /> </a>
+	<div class="navWrapper">
+		<a href="/">Home</a>
+		<a href="/directory">Directory</a>
+		<a href="/about">About Us</a>
+	</div>
+	<a href="/events">
+		<div class="events">
+			<Calendars />
+			Events
+		</div>
+	</a>
+</nav>
+
+{@render children()}
+
+<footer>
+	<div class="top">
+		<img src={logo} alt="logo" />
+		<p>ACSciS</p>
+	</div>
+
+	<div class="middle">
+		<div class="left">
+			<p>Contact Us</p>
+
+			<div class="contact-infos">
+				<div class="contact-info">
+					<Mail />
+					acscis2018.bupolangui@gmail.com
+				</div>
+				<div class="contact-info">
+					<Phone />
+					+63949 352 0227
+				</div>
+				<div class="contact-info">
+					<Icon data={facebookOfficial} scale={1.5} />
+					@ACSciSBUPolangui
+				</div>
+			</div>
+		</div>
+		<div class="right">
+			<p>Quick Links</p>
+			<a href="/directory">Directory</a>
+			<a href="/about">About Us</a>
+			<a href="/events">Events</a>
+		</div>
+	</div>
+
+	<div class="bottom">
+		<p>&#169; ACSciS 2026. All Rights Reserved.</p>
+	</div>
+</footer>
+
+<style>
+	nav,
+	div.navWrapper,
+	.events,
+	footer,
+	.top,
+	.middle,
+	.contact-info,
+	.left,
+	.right {
+		display: flex;
+	}
+
+	footer,
+	.top,
+	.left,
+	.right {
+		flex-direction: column;
+	}
+
+	nav {
+		gap: 1.5rem;
+		justify-content: space-between;
+		align-items: center;
+		padding: 1.4rem 0;
+	}
+
+	div.navWrapper {
+		gap: 2rem;
+	}
+
+	.logo img {
+		width: 60px;
+		height: 60px;
+	}
+
+	.events {
+		padding: 0.4rem 0.7rem;
+		gap: 0.65rem;
+		cursor: pointer;
+	}
+
+	footer,
+	footer a {
+		color: #a2a7b9;
+	}
+
+	footer {
+		gap: 2rem;
+		justify-content: center;
+		align-items: center;
+		padding-top: 5rem;
+		padding-bottom: 1rem;
+		border-top: 1px solid #3d415b;
+	}
+
+	.top {
+		gap: 0.45rem;
+		justify-content: center;
+		align-items: center;
+		margin-bottom: 1rem;
+	}
+
+	.top img {
+		height: 80px;
+		width: 80px;
+	}
+
+	.middle {
+		width: 100%;
+		justify-content: space-around;
+	}
+
+	.left {
+		gap: 0.75rem;
+		align-items: center;
+		justify-content: start;
+	}
+	.contact-info {
+		justify-content: center;
+		align-items: center;
+		gap: 0.45rem;
+	}
+
+	.top p,
+	.middle p {
+		color: var(--text);
+	}
+
+	.top p {
+		font-size: 1.5rem;
+		font-weight: bold;
+		font-family: 'Source Sans Pro';
+	}
+
+	.contact-infos {
+		display: flex;
+		flex-direction: column;
+		align-items: start;
+		gap: 0.45rem;
+	}
+
+	.right {
+		align-items: center;
+	}
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,129 @@
+<script>
+  import { Calendar, Rocket } from '@lucide/svelte';
+
+  let events = [
+    {
+      slug: 'code-start',
+      name: 'ACSciS Code Start',
+      date: 'March 6, 2026',
+      cover: '/imgs/events/code-start.jpg'
+    },
+    {
+      slug: 'kiritaan-kumsay',
+      name: 'Kiritaan Kumsay',
+      date: 'January 29, 2026',
+      cover: '/imgs/events/kiritaan.jpg'
+    },
+    {
+      slug: '2nd-GA',
+      name: '2nd General Assembly',
+      date: 'January 6, 2026',
+      cover: '/imgs/events/2ndga.jpg'
+    }
+  ];
+</script>
+
+<section class="hero">
+  <div class="banner">
+    <h1 class="banner-text">Ctrl + Logic: Where Ideas Merge, Algorithms Emerge</h1>
+    <h1 class="org">Association of Computer Science Students</h1>
+  </div>
+</section>
+
+<section class="recent-events">
+  <p>Recent Events</p>
+  <div class="events">
+    {#each events as event (event.slug)}
+      <a href="/events/{event.slug}">
+        <div class="event">
+          <img src={event.cover} alt={event.name} />
+          <div class="info">
+            <div class="date">
+              <Calendar size="18px" />
+              <p>{event.date}</p>
+            </div>
+            <div class="name">
+              <Rocket size="18px" />
+              <p>{event.name}</p>
+            </div>
+          </div>
+        </div>
+      </a>
+    {/each}
+  </div>
+</section>
+
+<style>
+  .hero {
+    display: flex;
+    flex-direction: column;
+    justify-content: end;
+    width: 100%;
+    height: 600px;
+    background-image: url('imgs/bg_home.jpeg');
+    background-size: cover;
+
+    margin: 2rem 0;
+    border: 2.5px solid var(--accent);
+  }
+
+  .banner {
+    background: #0d182ee3;
+
+    padding: 1rem 4rem;
+    border-radius: 0K.85rem;
+  }
+
+  .banner-text {
+    font-family: 'Cascadia Code';
+    font-size: 1.45rem;
+    color: var(--secondary);
+  }
+
+  .org {
+    color: var(--primary);
+  }
+
+  section.recent-events {
+    margin-bottom: 5rem;
+  }
+
+  .events {
+    margin-top: 1rem;
+    display: grid;
+    grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+    gap: 2rem;
+  }
+
+  .event {
+    display: flex;
+    flex-direction: column;
+    position: relative;
+    height: 290px;
+    background: #1c1f2a;
+  }
+
+  .info {
+    padding: 0.5rem 0.85rem;
+    display: flex;
+    flex-direction: column;
+    align-items: end;
+    gap: 0.4rem;
+  }
+
+  .date,
+  .name {
+    display: flex;
+    gap: 0.35rem;
+  }
+
+  .date {
+    font-size: 0.85rem;
+    color: var(--accent);
+  }
+
+  .name {
+    font-family: 'Cascadia Code';
+    color: var(--secondary);
+  }
+</style>
